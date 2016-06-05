e62896a14c1663b4c9ad5b8a8c386f3eef94f15d
Yuki Ishiguro <y.develop1496@gmail.com>
2016-06-05 14:47:54 +0900

    first commit

--- checkTest2/ViewController.swift
+++ checkTest2/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var textLabel :UILabel = UILabel()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +19,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch: UITouch = touches.first!
+        let location: CGPoint = touch.locationInView(self.view)
+        print(location)
+        textLabel = UILabel(frame: CGRectMake(location.x,location.y,100,50))
+        textLabel.text = "Check!"
+        self.view.addSubview(textLabel)
+    }
 
 }
 
