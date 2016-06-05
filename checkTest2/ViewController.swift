e1702bd55b00050ac9ac114cf57d0ed9672c69e0
Yuki Ishiguro <y.develop1496@gmail.com>
2016-06-05 12:17:59 +0900

    Initial Commit

---
+++ checkTest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  checkTest2
+//
+//  Created by yuki ishiguro on 2016/06/05.
+//  Copyright © 2016年 yuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
